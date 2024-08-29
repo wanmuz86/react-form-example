56b8069f575e5f1bc78ed3202677cc5c532d4933
Wan Muzaffar <wanmuz86@gmail.com>
2024-08-29 09:55:49 +0800

    uncontrolled form example

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 
 import './App.css'
 import ControlledExample from './components/ControlledExample'
+import UncontrolledExample from './components/UncontrolledExample'
 
 function App() {
 
 
   return (
     <>
-   <ControlledExample/>
+   {/* <ControlledExample/> */}
+   <UncontrolledExample/>
     </>
   )
 }
---
+++ src/components/UncontrolledExample.jsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react'
+
+const UncontrolledExample = () => {
+
+    // useRef = document.getElementById (getting the reference of a DOM element)
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(nameRef.current.value)
+        console.log(emailRef.current.value)
+    }
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input type="text" id='name' ref={nameRef} placeholder='Enter name'/>
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id='email' ref={emailRef} placeholder='Enter email'/>
+            </div>
+            <button type='submit'>Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default UncontrolledExample
